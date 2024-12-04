155a35a72822216961ab0414043c232c4204b5b3
francis <francis.vercruyssen@gmail.com>
2024-12-04 22:40:12 +0100

    add day 3

---
+++ 03-pt2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllText(args[0]);
+input = Regex.Replace(input, @"don't\(\).*?do\(\)", string.Empty, RegexOptions.Singleline);
+input = Regex.Replace(input, @"don't\(\).*", string.Empty, RegexOptions.Singleline);
+
+var result = 0;
+var matches = Regex.Matches(input, @"mul\((\d{1,3}),(\d{1,3})\)");
+
+foreach (Match match in matches)
+{
+    var a = int.Parse(match.Groups[1].Value);
+    var b = int.Parse(match.Groups[2].Value);
+    result += a * b;
+}
+
+Console.WriteLine(result);
---
+++ 03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+using var sr = new StreamReader(args[0]);
+
+var result = 0;
+while (sr.Peek() != -1)
+{
+    var line = sr.ReadLine();
+
+    var matches = Regex.Matches(line!, @"mul\((\d{1,3}),(\d{1,3})\)");
+    foreach (Match match in matches)
+    {
+        var a = int.Parse(match.Groups[1].Value);
+        var b = int.Parse(match.Groups[2].Value);
+        result += a * b;
+    }
+}
+
+Console.WriteLine(result);
