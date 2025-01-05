5241b69f2dfbbc89d89753de9d8c64d48f099470
francis <francis.vercruyssen@gmail.com>
2025-01-05 16:50:22 +0100

    add day 9

---
+++ 09-pt2/Program.cs
@@ -0,0 +1,75 @@
+﻿var input = File.ReadAllText(args[0]);
+
+var diskMap = input
+    .Where(x => x != '\n')
+    .Select(char.GetNumericValue)
+    .ToList();
+
+var disk = new List<int>();
+var i = 0;
+var fileId = 0;
+while (i < diskMap.Count)
+{
+    var fileLength = diskMap[i++];
+
+    for (var j = 0; j < fileLength; ++j)
+    {
+        disk.Add(fileId);
+    }
+
+    ++fileId;
+
+    if (i == diskMap.Count)
+    {
+        break;
+    }
+
+    var freeSpaceLength = diskMap[i++];
+
+    for (var j = 0; j < freeSpaceLength; ++j)
+    {
+        disk.Add(-1);
+    }
+}
+
+var compactedDisk = disk.ToList();
+
+for (var j = compactedDisk.Count - 1; j > -1; --j)
+{
+    if (compactedDisk[j] != -1)
+    {
+        var startIdx = compactedDisk.IndexOf(compactedDisk[j]);
+        var fileLength = j - startIdx + 1;
+
+        var emptyLength = 0;
+        for (var k = 0; k < startIdx; ++k)
+        {
+            if (compactedDisk[k] == -1)
+            {
+                ++emptyLength;
+                if (emptyLength == fileLength)
+                {
+                    for (var l = 0; l < fileLength; ++l)
+                    {
+                        compactedDisk[k - l] = compactedDisk[j - l];
+                        compactedDisk[j - l] = -1;
+                    }
+                    break;
+                }
+            }
+            else
+            {
+                emptyLength = 0;
+            }
+        }
+
+        j = startIdx;
+    }
+}
+
+var checksum = compactedDisk
+    .Select(x => x == -1 ? 0 : x)
+    .Select((val, idx) => (val, idx))
+    .Aggregate(0L, (a, b) => a + (b.val * b.idx));
+
+Console.WriteLine(checksum);
---
+++ 09/Program.cs
@@ -0,0 +1,58 @@
+﻿var input = File.ReadAllText(args[0]);
+
+var diskMap = input
+    .Where(x => x != '\n')
+    .Select(char.GetNumericValue)
+    .ToList();
+
+var disk = new List<int>();
+var i = 0;
+var fileId = 0;
+while (i < diskMap.Count)
+{
+    var fileLength = diskMap[i++];
+
+    for (var j = 0; j < fileLength; ++j)
+    {
+        disk.Add(fileId);
+    }
+
+    ++fileId;
+
+    if (i == diskMap.Count)
+    {
+        break;
+    }
+
+    var freeSpaceLength = diskMap[i++];
+
+    for (var j = 0; j < freeSpaceLength; ++j)
+    {
+        disk.Add(-1);
+    }
+}
+
+var compactedDisk = disk.ToList();
+
+for (var j = 0; j < compactedDisk.Count; ++j)
+{
+    if (compactedDisk[j] == -1)
+    {
+        var lastFileIdIdx = compactedDisk.FindLastIndex(x => x > -1);
+
+        if (lastFileIdIdx < j)
+        {
+            break;
+        }
+
+        compactedDisk[j] = compactedDisk[lastFileIdIdx];
+        compactedDisk[lastFileIdIdx] = -1;
+    }
+}
+
+var checksum = compactedDisk
+    .Where(x => x > -1)
+    .Select((val, idx) => (val, idx))
+    .Aggregate(0L, (a, b) => a + (b.val * b.idx));
+
+Console.WriteLine(checksum);
